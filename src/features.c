dc1bdd1e6ac2e3641f1dd997395597ebc138b7d8
gabkebab64 <gabriel.piankodulac@etu.estia.fr>
2025-05-16 15:00:37 +0200

    #1 implémentation de la fonction dimension

--- src/features.c
+++ src/features.c
@@ -14,3 +14,23 @@
 void helloWorld() {
     printf("Hello World !");
 }
+
+void dimension(char *source_path) {
+    unsigned char *data = NULL;
+    int width = 0, height = 0, channels = 0;
+
+    int result = read_image_data(source_path, &data, &width, &height, &channels);
+
+    if (result != 0) {
+        fprintf(stderr, "impossible de lire l'image "%s" \n", source_path);
+        return;
+    }
+
+    printf("dimension: %d, %d\n", width, height);
+
+    free(data);
+}
+
+
+
+}
